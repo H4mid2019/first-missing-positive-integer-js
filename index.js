c7247b92bb7f5b85a6e1886e305fff231a430490
H4mid2019 <47919732+H4mid2019@users.noreply.github.com>
2019-12-12 13:56:21 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-let n = [-1,-2,-3,-4,-5, 1,2,3,48,7,0,9,7,6,5,4,3,1]
-let n2 = n.filter(x => x > 0).sort((a, b) => a - b)
-n2 = new Set(n2)
-let c = 1
-for (let i of n2){
-  if(c != i ){
-    console.log(c)
-    break
-  }
-  c += 1
+
+function solution(n) {
+    let n2 = n.filter(x => x > 0).sort((a, b) => a - b)
+    n2 = new Set(n2)
+    let c = 1
+    for (let i of n2){
+      if(c != i ){
+        return c
+        break
+      }
+      c += 1
+    }
+    return c
 }
+
+console.log(solution([-1,-2,-3,-4,-5, 1,2,3,48,7,0,9,7,6,5,4,3,1]))
